64545ca7472aa2b5b0251a56edb6947aed5fec50
Alexander Khodzinsky <net1984master@gmail.com>
2022-05-16 14:23:11 +0300

    unit test add

--- src/review/review.repository.spec.ts
+++ src/review/review.repository.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReviewRepository } from './review.repository';
+import { getModelToken } from '@nestjs/mongoose';
+import { Schema, Types } from 'mongoose';
 
 describe('ReviewRepository', () => {
   let provider: ReviewRepository;
 
+  const exec = { exec: jest.fn() };
+  const reviewRepositoryFactory = () => ({
+    find: () => exec,
+  });
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ReviewRepository],
+      providers: [ReviewRepository,
+        { useFactory: reviewRepositoryFactory, provide: getModelToken('ReviewModel') }],
     }).compile();
 
     provider = module.get<ReviewRepository>(ReviewRepository);
@@ -14,5 +22,13 @@ describe('ReviewRepository', () => {
 
   it('should be defined', () => {
     expect(provider).toBeDefined();
+  });
+  it('findByProductId working', async () => {
+    const id = new Types.ObjectId().toHexString();
+    reviewRepositoryFactory()
+      .find()
+      .exec.mockReturnValueOnce([{ productId: id }]);
+    const res = await provider.findByProductId(id);
+    expect(res[0].productId).toBe(id);
   });
 });
